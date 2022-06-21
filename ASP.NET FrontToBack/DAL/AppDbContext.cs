11cfd49b752d75fd2753fc416f7857dc3e2bec53
SahilMamedov <sahilim@code.edu.az>
2022-06-21 13:00:08 +0400

    feat: add slider table and slidercontenttable

--- ASP.NET FrontToBack/DAL/AppDbContext.cs
+++ ASP.NET FrontToBack/DAL/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ASP.NET_FrontToBack.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +7,14 @@ using System.Threading.Tasks;
 
 namespace ASP.NET_FrontToBack.DAL
 {
-    public class AppDbContext:DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
         }
+
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<SliderContent> SliderContents { get; set; }
     }
 }
--- ASP.NET FrontToBack/Startup.cs
+++ ASP.NET FrontToBack/Startup.cs
@@ -1,6 +1,9 @@
+using ASP.NET_FrontToBack.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,11 +15,22 @@ namespace ASP.NET_FrontToBack
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config=config
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(option =>
+            {
+                option.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
